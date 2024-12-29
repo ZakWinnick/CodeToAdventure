9de05d26b57c0eb63845d1ad3c5bb07fea04a2a8
Zak Winnick <zak@winnick.io>
2024-12-28 16:55:44 -0800

    Update index-test.php

--- index-test.php
+++ index-test.php
@@ -405,7 +405,7 @@
                         <path d="M20 6h-2.18c.11-.31.18-.65.18-1a2.996 2.996 0 0 0-5.5-1.65l-.5.67-.5-.68C10.96 2.54 10.05 2 9 2 7.34 2 6 3.34 6 5c0 .35.07.69.18 1H4c-1.11 0-1.99.89-1.99 2L2 19c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V8c0-1.11-.89-2-2-2z"/>
                     </svg>
                     <div>
-                        <p class="reward-text">750 points that can be redeemed in Gear Shop or R1 Shop</p>
+                        <p class="reward-text">500 points that can be redeemed in Gear Shop or R1 Shop</p>
                         <p class="reward-note">(1 point equals 1 dollar in credit)</p>
                     </div>
                 </div>
@@ -428,7 +428,7 @@
         </p>
         <div class="footer-links">
             <a href='https://zak.codetoadventure.com' class="footer-link" target='_blank' rel='noopener noreferrer'>Zak's Referral Code</a>
-            <a href='changelog.html' class="footer-link" target='_blank' rel='noopener noreferrer'>Version 2024.12.24</a>
+            <a href='changelog.html' class="footer-link" target='_blank' rel='noopener noreferrer'>Version 2024.12.28</a>
             <a href="mailto:admin@codetoadventure.com" class="footer-link" target='_blank' rel='noopener noreferrer'>E-mail the admin</a>
         </div>
     </footer>
