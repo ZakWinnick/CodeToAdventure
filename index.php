7ddf49786aad571c75280a8f2525a5efbb67c4ea
Zak Winnick <zak@winnick.io>
2024-12-28 16:23:18 -0800

    Updated changelog and site version

--- index.php
+++ index.php
@@ -322,7 +322,7 @@
         </p>
         <div class="footer-links">
             <a href='https://zak.codetoadventure.com' class="footer-link" target='_blank' rel='noopener noreferrer'>Zak's Referral Code</a>
-            <a href='changelog.html' class="footer-link" target='_blank' rel='noopener noreferrer'>Version 2024.12.24</a>
+            <a href='changelog.html' class="footer-link" target='_blank' rel='noopener noreferrer'>Version 2024.12.28</a>
             <a href="mailto:admin@codetoadventure.com" class="footer-link" target='_blank' rel='noopener noreferrer'>E-mail the admin</a>
         </div>
     </footer>
