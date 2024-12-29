268c7d78f6ce5ec908606daecb42d29997fa4559
Zak Winnick <zak@winnick.io>
2024-12-28 17:20:32 -0800

    Update index.php

--- index.php
+++ index.php
@@ -371,7 +371,7 @@
     <nav class="nav-container">
         <div class="nav-content">
             <a href="index.php" class="nav-link">Home</a>
-            <a href="#" onclick="showModal(); return false;">Submit Your Code</a>
+            <a href="#" class="nav-link" onclick="showModal(); return false;">Submit Your Code</a>
             <a href="api-docs.html" class="nav-link">API Docs</a>
             <a href="changelog.html" class="nav-link">Changelog</a>
             <a href="/admin" class="nav-link">Admin</a>
