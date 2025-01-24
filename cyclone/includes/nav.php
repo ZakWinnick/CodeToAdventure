1e3be0624229fdc7cc287b2f4dfff866803e9b47
Zak Winnick <zak@winnick.io>
2025-01-24 08:59:45 -0800

    Update nav.php

--- cyclone/includes/nav.php
+++ cyclone/includes/nav.php
@@ -3,7 +3,7 @@
         <a href="index.php" class="nav-link">Home</a>
         <a href="#" class="nav-link" onclick="showModal(); return false;">Submit Code</a>
         <a href="api-docs.php" class="nav-link">API Docs</a>
-        <a href="changelog.html" class="nav-link">Changelog</a>
+        <a href="changelog.php" class="nav-link">Changelog</a>
         <a href="/admin" class="nav-link">Admin</a>
     </div>
 </nav>
