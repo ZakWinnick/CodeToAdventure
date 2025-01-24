34237f8ce2e0a150fedf90f77d8d494b74dfa1a5
Zak Winnick <zak@winnick.io>
2025-01-24 09:28:16 -0800

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -117,8 +117,8 @@ try {
         <div class="nav-content">
             <a href="../index.php" class="nav-link">Home</a>
             <a href="#" class="nav-link">Submit Code</a>
-            <a href="../api-docs.html" class="nav-link">API Docs</a>
-            <a href="../changelog.html" class="nav-link">Changelog</a>
+            <a href="../api-docs.php" class="nav-link">API Docs</a>
+            <a href="../changelog.php" class="nav-link">Changelog</a>
             <a href="logout.php" class="nav-link">Logout</a>
         </div>
     </header>
