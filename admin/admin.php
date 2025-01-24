c770e632fc5b87a7fc3a741a86a001329cf2ef2a
Zak Winnick <zak@winnick.io>
2025-01-24 09:28:48 -0800

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -116,7 +116,6 @@ try {
         <a href="/" class="logo-container">Code To Adventure</a>
         <div class="nav-content">
             <a href="../index.php" class="nav-link">Home</a>
-            <a href="#" class="nav-link">Submit Code</a>
             <a href="../api-docs.php" class="nav-link">API Docs</a>
             <a href="../changelog.php" class="nav-link">Changelog</a>
             <a href="logout.php" class="nav-link">Logout</a>
