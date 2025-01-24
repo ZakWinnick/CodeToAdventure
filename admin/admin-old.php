33edddfdc8e4484f29746a16b1e4c8d1e195bdbe
Zak Winnick <zak@winnick.io>
2025-01-24 09:39:11 -0800

    Update admin-old.php

--- admin/admin-old.php
+++ admin/admin-old.php
@@ -114,8 +114,8 @@ try {
 </head>
 
 <body>
-    <?php include 'includes/header.php'; ?>
-    <?php include 'includes/nav.php'; ?>
+    <?php include '../includes/header.php'; ?>
+    <?php include '../includes/nav.php'; ?>
 
 <!--<body>
     <header class="header">
