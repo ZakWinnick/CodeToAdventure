37cd1b5e26dc3747d1ab1ce5818b2d75eed13a69
Zak Winnick <zak@winnick.io>
2025-02-06 09:01:19 -0800

    Update changelog.php

--- changelog.php
+++ changelog.php
@@ -24,7 +24,18 @@ ini_set('display_errors', 1);
         </div>
 
         <div class="max-w-4xl mx-auto space-y-8">
-            <!-- Version 2025.7 -->
+
+            <div class="bg-gray-50 dark:bg-gray-800 border border-gray-400 dark:border-gray-700 rounded-xl p-6 shadow-xl transition-colors duration-200 text-left">
+                <h3 class="text-xl font-bold text-amber-600 dark:text-amber-400 mb-4">
+                    Version 2025.7.1 - February 6, 2025
+                </h3>
+                <ul class="list-disc ml-4 text-gray-900 dark:text-gray-300">
+                    <li>Fixed modal appearance on API Docs and Changelog pages</li>
+                    <li>Fixed a bug where you were not able to click/tap out of the Submit Code modal</li>
+                    <li>Centered toast notifications for better visibility</li>
+                </ul>
+            </div>
+
             <div class="bg-gray-50 dark:bg-gray-800 border border-gray-400 dark:border-gray-700 rounded-xl p-6 shadow-xl transition-colors duration-200 text-left">
                 <h3 class="text-xl font-bold text-amber-600 dark:text-amber-400 mb-4">
                     Version 2025.7 - February 5, 2025
