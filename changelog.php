2221865da06ef9b73c5c6a6b2c82aef5d7af8a48
Zak Winnick <zak@winnick.io>
2025-03-26 13:22:07 -0700

    2025.8 updates

--- changelog.php
+++ changelog.php
@@ -25,6 +25,17 @@ ini_set('display_errors', 1);
 
         <div class="max-w-4xl mx-auto space-y-8">
 
+
+        <div class="bg-gray-50 dark:bg-gray-800 border border-gray-400 dark:border-gray-700 rounded-xl p-6 shadow-xl transition-colors duration-200 text-left">
+                <h3 class="text-xl font-bold text-amber-600 dark:text-amber-400 mb-4">
+                    Version 2025.8 - March 26, 2025
+                </h3>
+                <ul class="list-disc ml-4 text-gray-900 dark:text-gray-300">
+                    <li>Due to changes in the Rivian Referral program, sharing of codes is no longer allowed. </li>
+                    <li>Updated the site to reflect the changes in the program.</li>                
+                </ul>
+            </div>
+
         <div class="bg-gray-50 dark:bg-gray-800 border border-gray-400 dark:border-gray-700 rounded-xl p-6 shadow-xl transition-colors duration-200 text-left">
                 <h3 class="text-xl font-bold text-amber-600 dark:text-amber-400 mb-4">
                     Version 2025.7.3 - March 11, 2025
