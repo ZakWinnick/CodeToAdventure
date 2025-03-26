7e233dd5ef2b680685f4e315e5086800bb6a06cc
Zak Winnick <zak@winnick.io>
2025-03-26 13:50:56 -0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     <div class="max-w-7xl mx-auto px-4 py-8">
         <div class="flex flex-col items-center text-center md:flex-row md:justify-between">
             <p class="text-gray-600 dark:text-gray-300 mb-4 md:mb-0">
-                © <?php echo date('Y'); ?> <a href="https://surfchaos.com" target="_blank">SurfChaos Media</a> & <a href="https://winnick.io" target="_blank">Zak Winnick</a>
+                © <?php echo date('Y'); ?> <a href="https://surfchaos.com" target="_blank">SurfChaos Media</a> / <a href="https://winnick.io" target="_blank">Zak Winnick</a>
             </p>
             <div class="flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-6">
                 <a href="https://zak.codetoadventure.com" class="text-gray-600 dark:text-gray-300 hover:text-primary dark:hover:text-accent transition-colors duration-200">Zak's Referral Code</a>
